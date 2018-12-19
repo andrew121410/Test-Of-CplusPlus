8dadbb94592273027483efb930da4b1d8817b58b
andrew121410 <andrewcherring@gmail.com>
2018-12-18 22:04:40 -0500

    update

--- Test-Of-CplusPlus/Test-Of-CplusPlus.cpp
+++ Test-Of-CplusPlus/Test-Of-CplusPlus.cpp
@@ -9,27 +9,47 @@ using namespace std;
 
 //FUNCTIONS
 
-void inputfunction(string test) {
+void testfunction(string test) {
 	cout << "input fuction got " << test << endl;
+}
+
+string whatisyournamefunction() {
+	string name;
+	cout << "Enter your name!" << endl;
+	//cin >> name;
+	//OR
+	getline(cin, name);
+	//cout << "Your name is " << name << endl;
+	return name;
 }
 
 //MAIN FUNCTION
 int main()
 {
 	//VAR EXAMPELS
+	string TEMP;
 	string testvarstring = "test";
 	int testvarint = 1;
 	char testvarchar = 'A';
 	double testvardouble = 1.2222222222;
 	bool isTest = true;
-		// END
+
+	int listint[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	string liststring[] = { "hello ", "my ", "name ", "is ", "andrew " };
+		// END OF VAR EXAMPLES
 
 	std::cout << "Just an Example!" << endl;
 
 	cout << "this is a " << testvarstring << endl; //<-- you can use this OR
 	std::cout << "this is a " << testvarstring << endl; //<-- this.
 
-	inputfunction(testvarstring);
+	//RUN FUNCTIONS
+	testfunction(testvarstring);
+	TEMP = whatisyournamefunction();
+	//END
+	liststring[4] = TEMP; //CHANGES 4 thing in the list too TMP;
+	cout << liststring[0] << liststring[1] << liststring[2] << liststring[3] << liststring[4] << endl; //HOPE you get it xD
+
 	return EXIT_SUCCESS; //Or you can return 0; TOO
 
 }
